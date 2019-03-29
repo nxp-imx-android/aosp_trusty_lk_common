d91a17c7dd87df4c72c85988b42c4e19026fb74f
Nick Bray <ncbray@google.com>
2019-03-29 13:23:26 -0700

    [arch][arm] reorganize how arch flags are generated
    Also enable neon and crypto support in user space.
    
    Bug: 125023379
    Change-Id: I64fec13d7a0f6db319c0f840a89204d3d162c1bf

--- arch/arm/toolchain.mk
+++ arch/arm/toolchain.mk
@@ -41,26 +41,36 @@ endif
 
 ARCH_arm_COMPILEFLAGS :=
 
-ifeq ($(ARM_CPU),cortex-a7)
-ARCH_arm_COMPILEFLAGS += -mcpu=$(ARM_CPU)
-ARCH_arm_COMPILEFLAGS += -mfpu=vfpv3 -mfloat-abi=softfp
-endif
-ifeq ($(ARM_CPU),cortex-a15)
-ARCH_arm_COMPILEFLAGS += -mcpu=$(ARM_CPU)
-ifneq ($(ARM_WITHOUT_VFP_NEON),true)
-ARCH_arm_COMPILEFLAGS += -mfpu=vfpv3 -mfloat-abi=softfp
-else
-ARCH_arm_COMPILEFLAGS += -mfloat-abi=soft
-endif
-endif
+# Arch
 ifeq ($(ARM_CPU),armv8-a)
 ARCH_arm_COMPILEFLAGS += -march=$(ARM_CPU)
-ifneq ($(ARM_WITHOUT_VFP_NEON),true)
-ARCH_arm_COMPILEFLAGS += -mfpu=vfpv3 -mfloat-abi=softfp
 else
+ARCH_arm_COMPILEFLAGS += -mcpu=$(ARM_CPU)
+endif
+
+# Floating point support
+ifneq ($(ARM_WITHOUT_VFP_NEON),true)
+# ARM_WITHOUT_VFP_NEON = false
+ifeq (false,$(call TOBOOL,$(ALLOW_FP_USE)))
+# This is likely kernel space.
+# Don't use neon registers but still support FP ASM.
+# The kernel will not save NEON register on interrupt.
+ARCH_arm_COMPILEFLAGS += -mfpu=vfpv3 -mfloat-abi=softfp -DWITH_NO_FP=1
+else # ALLOW_FP_USE = true
+# This is likely userspace.
+ifeq ($(ARM_CPU),cortex-a7)
+ARCH_arm_COMPILEFLAGS += -mfpu=neon-vfpv4 -mfloat-abi=softfp
+endif
+ifeq ($(ARM_CPU),cortex-a15)
+ARCH_arm_COMPILEFLAGS += -mfpu=neon-vfpv4 -mfloat-abi=softfp
+endif
+ifeq ($(ARM_CPU),armv8-a)
+ARCH_arm_COMPILEFLAGS += -mfpu=crypto-neon-fp-armv8 -mfloat-abi=softfp
+endif
+endif # ALLOW_FP_USE
+else # ARM_WITHOUT_VFP_NEON = true
 ARCH_arm_COMPILEFLAGS += -mfloat-abi=soft
-endif
-endif
+endif # ARM_WITHOUT_VFP_NEON
 
 ifeq ($(call TOBOOL,$(CLANGBUILD)),true)
 
