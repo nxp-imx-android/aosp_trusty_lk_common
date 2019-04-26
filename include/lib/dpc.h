bc552388ef64cdacf3aeab60d7ddd2dee68d8701
Michael Ryleev <gmar@google.com>
2019-04-26 14:16:50 -0700

    [lib][dpc] Rewrite DPC implementation
    The DPC (deferred procedure call) service in LK allows to queue
    work that will be executed at later time in context of DPC kernel
    thread. This CL reanimates this service (it does not compile as is)
    to use on devices with small memory footprint.
    
    Bug: 134153475
    Change-Id: I4dded43e6ba8a83204f51c39a8e07a47f2d0d621

--- include/lib/dpc.h
+++ include/lib/dpc.h
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2008 Travis Geiselbrecht
+ * Copyright (c) 2019 Google, Inc.
  *
  * Permission is hereby granted, free of charge, to any person obtaining
  * a copy of this software and associated documentation files
@@ -20,17 +21,105 @@
  * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
  * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
-#ifndef __KERNEL_DPC_H
-#define __KERNEL_DPC_H
+#pragma once
 
-#include <list.h>
+#include <lk/compiler.h>
+#include <lk/list.h>
+#include <kernel/thread.h>
 #include <sys/types.h>
 
-typedef void (*dpc_callback)(void *arg);
+__BEGIN_CDECLS
 
-#define DPC_FLAG_NORESCHED 0x1
+struct dpc;
 
-status_t dpc_queue(dpc_callback, void *arg, uint flags);
+/**
+ * typedef dpc_callback - DPC callback routine
+ * @work: pointer to &struct dpc item for which this callback routine
+ * is invoked.
+ *
+ * It is expected that &struct dpc item specified by @work parameter
+ * is embedded into a caller specific structure and the caller will be
+ * using the containerof macro to recover the outer structure.
+ *
+ * Return: none
+ */
+typedef void (*dpc_callback)(struct dpc* work);
 
-#endif
+/**
+ * struct dpc_queue - opaque DPC queue tracking structure
+ */
+struct dpc_queue;
 
+/**
+ * struct dpc - DPC work item tracking structure
+ * @node: tracking list node
+ * @cb:  pointer to callback routine to invoke
+ * @q: pointer to DPC queue if DPC item is queued
+ */
+struct dpc {
+    /* private: internal use only */
+    struct list_node node;
+    dpc_callback cb;
+    struct dpc_queue* q;
+};
+
+/**
+ * dpc_work_init() - initialize specified DPC work item
+ * @work: pointer to &struct dpc to initialize
+ * @cb: callback to invoke
+ * @flags: reserved must be 0
+ *
+ * Return: none
+ */
+void dpc_work_init(struct dpc* work, dpc_callback cb, uint32_t flags);
+
+/**
+ * dpc_enqueue_work(): enqueue DPC work to run on specified DPC queue
+ * @q: DPC queue to run DPC work specified by @work parameter. If @q is NULL
+ * the work will be enqueued in the default DPC queue.
+ * @work: DPC work to enqueue on DPC queue specified by @q parameter
+ * @resched: directly passed to underlying event_signal() call. (See the
+ * description of event_signal() call for more details).
+ *
+ * Note 1: It is guaranteed that after each invocation of dpc_enqueue_work()
+ * routine the corresponding DPC work item will be processed at least once.
+ *
+ * Note 2: A DPC work item may be re-enqueued the same DPC work queue it is
+ * already in. This is a no op.
+ *
+ * Note 3: The work item is removed from the DPC queue before the callback is
+ * invoked. It is safe to re-enqueue DPC work item inside its own callback.
+ *
+ * Note 4: If DPC work item is enqueued again after it has been removed from
+ * the queue but before callback is invoked or while callback is running it is
+ * guaranteed that DPC work item will be processed again in the future.
+ *
+ * Note 5: An initialized DPC work item can only be enqueued in one queue.
+ * An item needs to be initialized again in order to be enqueued in another
+ * queue.
+ *
+ * Note 6: The only time when it is safe to free or reinitialize a DPC work
+ * item is when it is not queued, for example, from DPC callback itself.
+ *
+ * Note 7: The @resched parameter must be false if invoked from interrupt
+ * context.
+ *
+ * return value: 0 on success
+ */
+int dpc_enqueue_work(struct dpc_queue* q, struct dpc* work, bool resched);
+
+/**
+ * dpc_queue_start(): initialize and start DPC queue
+ * @name: DPC queue name
+ * @thread_priority: a priority of DPC queue handling thread
+ * @thread_stack_size: stack size of DPC queue handling thread
+ * DEFAULT_STACK_SIZE is a reasonable default value here.
+ *
+ * Return: NO_ERROR on success, a negative error code otherwise
+ */
+status_t dpc_queue_start(struct dpc_queue* q,
+                         const char* name,
+                         int thread_priority,
+                         size_t thread_stack_size);
+
+__END_CDECLS
--- lib/dpc/dpc.c
+++ lib/dpc/dpc.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2008 Travis Geiselbrecht
+ * Copyright (c) 2019 Google, Inc.
  *
  * Permission is hereby granted, free of charge, to any person obtaining
  * a copy of this software and associated documentation files
@@ -20,76 +21,117 @@
  * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
  * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
+#include <assert.h>
 #include <debug.h>
-#include <stddef.h>
-#include <list.h>
-#include <malloc.h>
-#include <err.h>
-#include <lib/dpc.h>
-#include <kernel/thread.h>
 #include <kernel/event.h>
+#include <kernel/spinlock.h>
+#include <kernel/thread.h>
+#include <lib/dpc.h>
 #include <lk/init.h>
+#include <lk/list.h>
+#include <malloc.h>
+#include <stddef.h>
+#include <trace.h>
+#include <uapi/err.h>
 
-struct dpc {
-    struct list_node node;
+#define LOCAL_TRACE 0
 
-    dpc_callback cb;
-    void *arg;
+struct dpc_queue {
+    struct list_node list;
+    struct event event;
+    spin_lock_t lock;
+    struct thread* thread;
 };
 
-static struct list_node dpc_list = LIST_INITIAL_VALUE(dpc_list);
-static event_t dpc_event;
+static struct dpc_queue default_queue;
 
-static int dpc_thread_routine(void *arg);
+static int dpc_thread_routine(void* arg);
 
-status_t dpc_queue(dpc_callback cb, void *arg, uint flags)
-{
-    struct dpc *dpc;
+void dpc_work_init(struct dpc* work, dpc_callback cb, uint32_t flags) {
+    ASSERT(work);
 
-    dpc = malloc(sizeof(struct dpc));
-
-    if (dpc == NULL)
-        return ERR_NO_MEMORY;
-
-    dpc->cb = cb;
-    dpc->arg = arg;
-    enter_critical_section();
-    list_add_tail(&dpc_list, &dpc->node);
-    event_signal(&dpc_event, (flags & DPC_FLAG_NORESCHED) ? false : true);
-    exit_critical_section();
-
-    return NO_ERROR;
+    list_clear_node(&work->node);
+    work->cb = cb;
+    work->q = NULL;
 }
 
-static int dpc_thread_routine(void *arg)
-{
+int dpc_enqueue_work(struct dpc_queue* q, struct dpc* work, bool resched) {
+    spin_lock_saved_state_t state;
+
+    ASSERT(work);
+    ASSERT(work->cb);
+
+    if (!q) {
+        q = &default_queue;
+    }
+
+    spin_lock_irqsave(&q->lock, state);
+    ASSERT(!work->q || (work->q == q));
+    if (!list_in_list(&work->node)) {
+        list_add_tail(&q->list, &work->node);
+        work->q = q;
+    }
+    spin_unlock_irqrestore(&q->lock, state);
+    event_signal(&q->event, resched);
+    return 0;
+}
+
+static int dpc_thread_routine(void* arg) {
+    struct dpc* work;
+    struct dpc_queue* q = arg;
+    spin_lock_saved_state_t state;
+
+    DEBUG_ASSERT(q);
+
     for (;;) {
-        event_wait(&dpc_event);
+        event_wait(&q->event);
+        do {
+            spin_lock_irqsave(&q->lock, state);
+            work = list_remove_head_type(&q->list, struct dpc, node);
+            spin_unlock_irqrestore(&q->lock, state);
 
-        enter_critical_section();
-        struct dpc *dpc = list_remove_head_type(&dpc_list, struct dpc, node);
-        if (!dpc)
-            event_unsignal(&dpc_event);
-        exit_critical_section();
-
-        if (dpc) {
-//          dprintf("dpc calling %p, arg %p\n", dpc->cb, dpc->arg);
-            dpc->cb(dpc->arg);
-
-            free(dpc);
-        }
+            if (work) {
+                LTRACEF("dpc calling %p\n", work->cb);
+                work->cb(work);
+            }
+        } while (work);
     }
 
     return 0;
 }
 
-static void dpc_init(uint level)
-{
-    event_init(&dpc_event, false, 0);
+status_t dpc_queue_start(struct dpc_queue* q,
+                         const char* name,
+                         int thread_priority,
+                         size_t thread_stack_size) {
+    DEBUG_ASSERT(q);
+    DEBUG_ASSERT(!q->thread);
 
-    thread_detach_and_resume(thread_create("dpc", &dpc_thread_routine, NULL, DPC_PRIORITY, DEFAULT_STACK_SIZE));
+    /* Initiliaze queue */
+    spin_lock_init(&q->lock);
+    list_initialize(&q->list);
+    event_init(&q->event, false, EVENT_FLAG_AUTOUNSIGNAL);
+
+    /* create thread */
+    q->thread = thread_create(name, dpc_thread_routine, q, thread_priority,
+                              thread_stack_size);
+    if (!q->thread)
+        return ERR_NO_MEMORY;
+
+    /* start thread */
+    thread_detach_and_resume(q->thread);
+    return 0;
+}
+
+static void dpc_init(uint level) {
+    status_t rc;
+
+    /* init and start default DPC queue */
+    rc = dpc_queue_start(&default_queue, "dpc", DPC_PRIORITY,
+                         DEFAULT_STACK_SIZE);
+    if (rc != NO_ERROR) {
+        panic("failed to start default dpc queue\n");
+    }
 }
 
 LK_INIT_HOOK(libdpc, &dpc_init, LK_INIT_LEVEL_THREADING);
-
-
