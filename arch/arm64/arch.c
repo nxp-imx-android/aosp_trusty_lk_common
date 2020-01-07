a8df67b9d5e8752bd0abef650716b4cb652c599d
Haoran.Wang <elven.wang@nxp.com>
2020-01-07 09:49:44 +0800

    MA-15300 change CPU id if we use A72 boot for imx8qm
    When WITH_BOOT_FROM_A72 set, change first A72 core as CPU0,
    then mark cluster A53's 4 cores as CPU1 to CPU4. The second A72 still
    be CPU5.
    This patch support A72 boot first to make boot faster for Android
    Automotive.
    
    Change-Id: I04359c49e6972da0e1b67df22aab7e9ea99552ae
    Signed-off-by: Haoran.Wang <elven.wang@nxp.com>

--- arch/arm64/arch.c
+++ arch/arm64/arch.c
@@ -169,8 +169,10 @@ void arch_enter_uspace(vaddr_t entry_point, vaddr_t user_stack_top, uint32_t fla
 void arm64_secondary_entry(ulong asm_cpu_num)
 {
     uint cpu = arch_curr_cpu_num();
+#ifndef WITH_BOOT_FROM_A72
     if (cpu != asm_cpu_num)
         return;
+#endif
 
     arm64_cpu_early_init();
 
