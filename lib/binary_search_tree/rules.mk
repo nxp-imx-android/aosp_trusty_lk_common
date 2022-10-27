38d109f8e245e73008669a877b5e18f79801d44b
David Anekstein <anekstein@google.com>
2022-10-27 17:44:26 -0400

    lib: binary_search_tree: expose as library
    Bug: 260484725
    Change-Id: Ic6bc8f034007cdc441b908014671b66281873986

--- lib/binary_search_tree/rules.mk
+++ lib/binary_search_tree/rules.mk
@@ -2,7 +2,10 @@ LOCAL_DIR := $(GET_LOCAL_DIR)
 
 MODULE := $(LOCAL_DIR)
 
+MODULE_EXPORT_INCLUDES += \
+	$(LOCAL_DIR)/include
+
 MODULE_SRCS += \
 	$(LOCAL_DIR)/binary_search_tree.c
 
-include make/module.mk
+include make/library.mk
